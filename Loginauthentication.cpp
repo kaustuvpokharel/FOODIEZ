2c28f4ae04a5022d90d15e944e133386e41c9ab3
Kaustuv Pokharel <kastuvpokharel@gmail.com>
2025-03-21 18:22:21 -0400

    login authetication class initialized

---
+++ Loginauthentication.cpp
@@ -0,0 +1,3 @@
+#include "Loginauthentication.h"
+
+LoginAuth::LoginAuth() {}
---
+++ Loginauthentication.h
@@ -0,0 +1,13 @@
+#ifndef LOGINAUTH_H
+#define LOGINAUTH_H
+
+#include <QObject>
+
+class LoginAuth
+{
+    Q_OBJECT
+public:
+    LoginAuth();
+};
+
+#endif // LOGINAUTH_H
