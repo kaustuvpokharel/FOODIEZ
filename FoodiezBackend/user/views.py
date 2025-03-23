b151e0e50d57480d4f4f3fb33084a238592c9f24
Bhaskar Neupane <bhaskar@vaskrneup.com>
2025-03-22 22:50:56 -0400

    create API endpoint for authentication and getting user profile

--- FoodiezBackend/user/views.py
+++ FoodiezBackend/user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from .serializers import CustomTokenObtainPairSerializer
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+class UserProfileView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
