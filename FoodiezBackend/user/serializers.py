121bda08a81a18c116c76d3af796dca56689ea16
Bhaskar Neupane <bhaskar@vaskrneup.com>
2025-03-22 23:31:19 -0400

    implement login using both username and pw & registration api added

--- FoodiezBackend/user/serializers.py
+++ FoodiezBackend/user/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework import serializers
+from django.conf import settings
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
 
 User = get_user_model()
 
@@ -11,11 +11,57 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'location', 'website', 'slug']
 
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+
+
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        # Add custom claims
-        token['username'] = user.username
-        token['is_verified'] = user.is_verified
-        return token
+    username_field = 'username'  # Still required by parent class
+
+    def validate(self, attrs):
+        username_or_email = attrs.get('username')
+        password = attrs.get('password')
+
+        user: User = authenticate(username=username_or_email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError('Invalid credentials')
+
+        data = super().validate({
+            'username': user.username,  # NOQA
+            'password': password
+        })
+
+        data['user'] = {  # NOQA
+            'id': user.id,
+            'username': user.username,  # NOQA
+            'email': user.email,  # NOQA
+            'is_verified': user.is_verified  # NOQA
+        }
+
+        return data
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'bio', 'location', 'website', 'profile_picture']
+
+    def create(self, validated_data):
+        profile_picture = validated_data.get('profile_picture')
+        if not profile_picture:
+            validated_data['profile_picture'] = settings.DEFAULT_PROFILE_PICTURE
+
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            location=validated_data.get('location', ''),
+            website=validated_data.get('website', ''),
+            profile_picture=validated_data['profile_picture'],
+        )
+        return user
